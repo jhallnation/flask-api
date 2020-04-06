f06c3bc45fd03c3c1e6813b45409cef9f38a461c
Jason Hall <jhallnation@gmail.com>
2020-04-06 14:54:06 -0600

    guide destroy route

--- app.py
+++ app.py
@@ -72,6 +72,16 @@ def guide_update(id):
 
   return guide_schema.jsonify(guide)
 
+@app.route('/guide/<id>', methods=['DELETE'])
+def guide_delete(id):
+  guide = Guide.query.get(id)
+
+  db.session.delete(guide)
+  db.session.commit()
+
+  return guide_schema.jsonify(guide)
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
